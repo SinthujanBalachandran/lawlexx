e9e5d39f94788ed22a55740a79d34f83b5b8920d
SinthujanBalachandran <54698571+SinthujanBalachandran@users.noreply.github.com>
2021-09-20 13:44:53 +0530

    jquery counter fixed

---
+++ navigation.js
@@ -0,0 +1,21 @@
+// navigation scroll
+
+window.addEventListener('scroll', function() {
+    const header = document.querySelector('header');
+    const toggle = document.querySelector('.toggle');
+    header.classList.toggle("sticky", window.scrollY > 0);
+    toggle.classList.toggle("sticky", window.scrollY > 0);
+});
+
+// media screen navigation
+
+const navBar = document.querySelector('.navigation');
+const toggleBtn = document.querySelector('.toggle');
+toggleBtn.onclick = function() {
+    toggleBtn.classList.toggle('active');
+    navBar.classList.toggle('active');
+}
+document.querySelector('.navigation').onclick = function() {
+    toggleBtn.classList.toggle('active');
+    navBar.classList.toggle('active');
+}
