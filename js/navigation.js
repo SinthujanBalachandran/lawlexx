4660e175b8c3a9e313b3313c8065dac6ad76c794
SinthujanBalachandran <54698571+SinthujanBalachandran@users.noreply.github.com>
2021-09-27 21:52:20 +0530

    new nav, slider

--- js/navigation.js
+++ js/navigation.js
@@ -1,21 +1,42 @@
+//jquery for toggle dropdown menus
+$(document).ready(function() {
+    //toggle sub-menus
+    $(".sub-btn").click(function() {
+        $(this).next(".sub-menu").slideToggle();
+    });
+});
+const menu = document.querySelector('.menu');
+const menuBtn = document.querySelector('.menu-btn');
+menuBtn.onclick = function() {
+        menuBtn.classList.toggle('active');
+        menu.classList.toggle('active');
+
+    }
+    //javascript for the navigation bar effects on scroll
+window.addEventListener("scroll", function() {
+    var header = document.querySelector("header");
+    header.classList.toggle("sticky", window.scrollY > 0);
+});
+
+
 // navigation scroll
 
-window.addEventListener('scroll', function() {
-    const header = document.querySelector('header');
-    const toggle = document.querySelector('.toggle');
-    header.classList.toggle("sticky", window.scrollY > 0);
-    toggle.classList.toggle("sticky", window.scrollY > 0);
-});
+// window.addEventListener('scroll', function() {
+//     const header = document.querySelector('header');
+//     const toggle = document.querySelector('.toggle');
+//     header.classList.toggle("sticky", window.scrollY > 0);
+//     toggle.classList.toggle("sticky", window.scrollY > 0);
+// });
 
 // media screen navigation
 
-const navBar = document.querySelector('.navigation');
-const toggleBtn = document.querySelector('.toggle');
-toggleBtn.onclick = function() {
-        toggleBtn.classList.toggle('active');
-        navBar.classList.toggle('active');
-    }
-    // navBar.onclick = function() {
-    //     toggleBtn.classList.toggle('active');
-    //     navBar.classList.toggle('active');
-    // }
+// const navBar = document.querySelector('.navigation');
+// const toggleBtn = document.querySelector('.toggle');
+// toggleBtn.onclick = function() {
+//         toggleBtn.classList.toggle('active');
+//         navBar.classList.toggle('active');
+//     }
+// navBar.onclick = function() {
+//     toggleBtn.classList.toggle('active');
+//     navBar.classList.toggle('active');
+// }
